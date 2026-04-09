1e24e6bf7b0ea35f0178cc9509f630342987294e
3iron <3iron38@gmail.com>
2026-04-09 13:32:08 +0900

    Nav 바 모바일 반응형 추가

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,23 +3,24 @@
     <div class="container-fluid">
       <RouterLink class="navbar-brand" to="/">우주를 줄게</RouterLink>
 
-      <div class="navbar-nav ms-auto">
-        <RouterLink v-if="!isLogin" class="nav-link" to="/loginPage">
-          로그인
-        </RouterLink>
+      <nav class="header-menu">
+        <div class="navbar-nav ms-auto">
+          <RouterLink v-if="!isLogin" class="nav-link" to="/loginPage">
+            로그인
+          </RouterLink>
 
-        <template v-else>
-          <RouterLink class="nav-link" to="/mypage">마이페이지</RouterLink>
-          <button class="nav-link btn btn-link" @click="logout">
-            로그아웃
-          </button>
-        </template>
-
-        <RouterLink class="nav-link" to="/transactionList">목록</RouterLink>
-        <RouterLink class="nav-link" to="/transactionForm">거래등록</RouterLink>
-        <RouterLink class="nav-link" to="/summaryCard">통계</RouterLink>
-        <RouterLink class="nav-link" to="/budgetCard">예산</RouterLink>
-      </div>
+          <template v-else>
+            <RouterLink class="nav-link" to="/mypage">마이페이지</RouterLink>
+            <button class="nav-link btn btn-link" @click="logout">
+              로그아웃
+            </button>
+          </template>
+          <RouterLink class="nav-link" to="/transactionList">홈</RouterLink>
+          <!-- <RouterLink class="nav-link" to="/transactionForm">거래등록</RouterLink> -->
+          <RouterLink class="nav-link" to="/summaryCard">통계</RouterLink>
+          <RouterLink class="nav-link" to="/budgetCard">예산</RouterLink>
+        </div>
+      </nav>
     </div>
   </nav>
 </template>
